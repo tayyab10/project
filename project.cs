9b34add3f467f908f05ab45e2845c5143368b4a0
tayyab10 <98313037+tayyab10@users.noreply.github.com>
2022-02-13 22:58:43 +0500

    Create project.cs

---
+++ project.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorldApp
+{
+    class Project1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+
